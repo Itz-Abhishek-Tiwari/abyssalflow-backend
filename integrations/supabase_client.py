66517834b03f65801ad5ce324c5e6c932b0f1e71
Abhishek Tiwari <abhitiwariabhi5@gmail.com>
2025-09-26 13:02:50 +0530

    added debuging

--- integrations/supabase_client.py
+++ integrations/supabase_client.py
@@ -5,7 +5,7 @@ import time
 from supabase import create_client, Client
 
 SUPABASE_URL = os.environ.get("SUPABASE_URL")
-SUPABASE_KEY = os.environ.get("SUPABASE_SERVICE_ROLE_KEY")
+SUPABASE_KEY = os.environ.get("SUPABASE_SERVICE_KEY")
 
 if not SUPABASE_URL or not SUPABASE_KEY:
     raise Exception(
